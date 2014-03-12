cf7863dd74ce943ba717fa1b419ac03a827fa4be
Aayush Dawra <aayushgopaldawra@gmail.com>
2014-03-12 11:00:12 -0700

    Skeleton commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sid2014::Application.config.secret_token = 'e2d0bf3c91a77613338f73b36e6d54bd97da13568bf56c1d038f6b1bffbe3f2612c7852ca0973fbaea7dfd3c135dd090609c37f18599d9c71fb9373fad3cd007'
